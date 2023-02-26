0ee15059c9c5b4a483c04197dccfb5b418867403
Michael Viernes <92641515+MichaelViernes271@users.noreply.github.com>
2023-02-26 11:35:47 +0800

    READ THE OBJECTIVES PLEASE

--- transcript_feature.py
+++ transcript_feature.py
@@ -1,3 +1,17 @@
+"""
+OBJECTIVES:
+(X) DISPLAYING THE USER DETAIL ABOVE
+(O) RETRIEVING THE DATA FROM CSV
+(O) DISPLAYING THE GRADES
+(O) DISPLAYING THE COURSE AND AVERAGE ORGANIZED BY TERM
+(X) PROPER FORMATTING FOR USER READABILITY
+(X) STORING THE USER TRANSCRIPT TO THE FILE
+
+NOTE: IN THE display_transcript(), IT 
+IS NOT FORMATTED YET LABELED MATCHING THE PROPER OUTPUT. PLEASE CHANGE THIS.
+
+"""
+
 import csv, math
 
 def transcriptFeature(filename, typeofcourse=None):
@@ -5,59 +19,58 @@ def transcriptFeature(filename, typeofcourse=None):
     with open(filename) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
 
-        # Initialize variables for major/minor courses and grades
-        row = csv_reader
-        courseid = []
-        coursename = []
-        credithours = []
-        grade = []
-        terms = []
-        courses = [x for x in row if x[5].lower() == typeofcourse]
+        # PSEUDOCODE
+        # select which is major and minor
+        courses = [x for x in csv_reader if x[5].lower() == typeofcourse]
+        # count the terms
+        terms = [x[2] for x in courses]
+        terms = int(max(terms))
+        print("max term: ", terms, type(terms))
+        for term in range(1, int(terms)+1):
+            # decorator(term) # delineates successive terms and marks the end
+            display_transcript(courses, term, typeofcourse)
 
-        # Read each row in the CSV file
-        # by the respective columns of vars based on majors
-        for x in courses:
-            coursename.append(x[3])
-            courseid.append(x[4])
-            grade.append(int(x[7]))
-            credithours.append(int(x[6]))
-            terms.append(int(x[2]))
-
-        overall_average = average(grade) # compute overall average in terms
-        
-        print("Course names:\n", coursename)
-        print("CourseIDs:\n", courseid)
-        print("Credit Hours:\n", credithours)
-        print("Grade:\n", grade)
-        print("Overall Average:\n", overall_average)
-
-        for term in range(1, max(terms)+1):
-            display_transcript(term, terms,
-                coursename, courseid, grade, credithours)
+        # testing vars
+        # print("Course names:\n", coursename)
+        # print("CourseIDs:\n", courseid)
+        # print("Credit Hours:\n", credithours)
+        # print("Grade:\n", grade)
+        # print("Overall Average:\n", overall_average)
 
 
-def display_transcript(term, terms, coursename, courseid, grade, credithours):
+def display_transcript(courses, term, typeofcourse):
     """print the transcript based on major and minor"""
 
+    # for storing the data in separate course each term
     grades = []
-    print(f"for the term {term}:")
-    for i in range(len(terms)):
-        if term == terms[i]:
-            # selecting grades of this term
-            grades.append(grade[i])
+    for course in courses:
+        if term == int(course[2]):
+            print("Student detail:\n", course)
+            # convert grades into int
+            grade = int(course[7])
+            grades.append(grade)
+    
+    subject_average = average(grades) # compute average
 
-            print(coursename[i])
-            print(courseid[i])
-            print(credithours[i])
-            print(grade[i])
+    # compute the average the term and overall terms
+    overall_average = average(grades)
 
-    print(f"average for this {term} sem: {average(grades)}", )
 
-def average(grade):
+    print("Overall Average:\n", overall_average)
+    print("{} Average:\n".format(typeofcourse.title()), subject_average)
+
+
+def average(grades):
     # rounded to 2 decimals by format
-    overall_average = round((math.fsum(grade)/len(grade)), 3)
+    overall_average = round((sum(grades)/len(grades)), 3)
     overall_average = f"{overall_average:4.2f}"
     return overall_average
+
+# # still working ....
+# def decorator(decors):
+#     """Puts a design around the transcript."""
+#     decoration = "="*60 + "{decors:^}".center(60, "*") + 
+#     print()
 
 if __name__ == "__main__":
     # testing
