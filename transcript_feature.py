4d46a885395723b0088299a5965919a4ced2aa26
Michael Viernes <92641515+MichaelViernes271@users.noreply.github.com>
2023-02-28 08:09:33 +0800

    Add files via upload

--- transcript_feature.py
+++ transcript_feature.py
@@ -83,7 +83,7 @@ def display_transcript(courses, term, typeofcourse):
     subject_average = average(grades) # compute average each term
     overall_average = average(grades) # compute  overall term average
 
-    text_average = "{} Average: ".format(typeofcourse.title())
+    text_average = "{} Average: {}".format(typeofcourse.title(), subject_average)
     print(text_average, end="\t")
     text_overall_average = f"Overall Average: {overall_average}"
     print(text_overall_average)
